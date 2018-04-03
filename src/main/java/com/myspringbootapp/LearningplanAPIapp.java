d1ef259a51ee92687323ad2cdbb17f7144b91a0f
Shama <shamaanjum09@outlook.com>
2018-04-02 17:40:46 -0700

    initial commit with pom.xml

---
+++ src/main/java/com/myspringbootapp/LearningplanAPIapp.java
@@ -0,0 +1,13 @@
+package com.myspringbootapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningplanAPIapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
